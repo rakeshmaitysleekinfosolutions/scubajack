8e6387329a333baf3c9425ede8bdbcdbad911239
soumisleek <soumi@sleekinfosolutions.com>
2020-08-31 15:59:19 +0530

    create product-details page and see-all page

--- application/modules/app/views/category/index.php
+++ application/modules/app/views/category/index.php
@@ -1,14 +1,111 @@
-<section class="loginpage">
-    <div class="formbox">
-        <div class="container">
-            <div class="row">
-                <h3>Lis of category</h3>
-
-            </div>
-        </div>
-    </div>
+<section class="skill-inner">
+	<div class="container">
+		<div class="logo"> <img src="http://localhost/scubajack/assets/images/scuba-logo.png"> </div>
+		<div class="skill-list">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="skillbox">
+						<img src="http://localhost/scubajack/image/cache/catalog/[adventuresofscubajack.com][199]Books-Home-page-1-400-534x205.jpg">
+						<div class="s-link">
+							<a href="http://localhost/scubajack/category/creatures-big-and-small" class="btn whitebtn">CREATURES BIG AND SMALL1</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="skillbox">
+						<img src="http://localhost/scubajack/image/cache/catalog/[adventuresofscubajack.com][250]element-534x205.jpg">
+						<div class="s-link">
+							<a href="http://localhost/scubajack/category/story-books" class="btn whitebtn">STORY BOOKS</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="skillbox">
+						<img src="http://localhost/scubajack/image/cache/catalog/[adventuresofscubajack.com][191]people-2-534x205.jpg">
+						<div class="s-link">
+							<a href="http://localhost/scubajack/category/learn-to-read" class="btn whitebtn">LEARN TO READ</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="skillbox">
+						<img src="http://localhost/scubajack/image/cache/catalog/[adventuresofscubajack.com][386]activity-534x205.jpg">
+						<div class="s-link">
+							<a href="http://localhost/scubajack/category/elementary-school-reading" class="btn whitebtn">ELEMENTARY SCHOOL READING</a>
+						</div>
+					</div>
+				</div>
+					<div class="col-md-6">
+					<div class="skillbox">
+						<img src="http://localhost/scubajack/image/cache/catalog/[adventuresofscubajack.com][199]Books-Home-page-1-400-534x205.jpg">
+						<div class="s-link">
+							<a href="http://localhost/scubajack/category/creatures-big-and-small" class="btn whitebtn">CREATURES BIG AND SMALL1</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="skillbox">
+						<img src="http://localhost/scubajack/image/cache/catalog/[adventuresofscubajack.com][250]element-534x205.jpg">
+						<div class="s-link">
+							<a href="http://localhost/scubajack/category/story-books" class="btn whitebtn">STORY BOOKS</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="skillbox">
+						<img src="http://localhost/scubajack/image/cache/catalog/[adventuresofscubajack.com][191]people-2-534x205.jpg">
+						<div class="s-link">
+							<a href="http://localhost/scubajack/category/learn-to-read" class="btn whitebtn">LEARN TO READ</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="skillbox">
+						<img src="http://localhost/scubajack/image/cache/catalog/[adventuresofscubajack.com][386]activity-534x205.jpg">
+						<div class="s-link">
+							<a href="http://localhost/scubajack/category/elementary-school-reading" class="btn whitebtn">ELEMENTARY SCHOOL READING</a>
+						</div>
+					</div>
+				</div>
+		<div class="col-md-6">
+					<div class="skillbox">
+						<img src="http://localhost/scubajack/image/cache/catalog/[adventuresofscubajack.com][199]Books-Home-page-1-400-534x205.jpg">
+						<div class="s-link">
+							<a href="http://localhost/scubajack/category/creatures-big-and-small" class="btn whitebtn">CREATURES BIG AND SMALL1</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="skillbox">
+						<img src="http://localhost/scubajack/image/cache/catalog/[adventuresofscubajack.com][250]element-534x205.jpg">
+						<div class="s-link">
+							<a href="http://localhost/scubajack/category/story-books" class="btn whitebtn">STORY BOOKS</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="skillbox">
+						<img src="http://localhost/scubajack/image/cache/catalog/[adventuresofscubajack.com][191]people-2-534x205.jpg">
+						<div class="s-link">
+							<a href="http://localhost/scubajack/category/learn-to-read" class="btn whitebtn">LEARN TO READ</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="skillbox">
+						<img src="http://localhost/scubajack/image/cache/catalog/[adventuresofscubajack.com][386]activity-534x205.jpg">
+						<div class="s-link">
+							<a href="http://localhost/scubajack/category/elementary-school-reading" class="btn whitebtn">ELEMENTARY SCHOOL READING</a>
+						</div>
+					</div>
+				</div>
+		
+			
+			</div>
+		</div>
+	</div>
 </section>
 <script>
-    var myLabel = myLabel || {};
-    myLabel.baseUrl = '<?php echo base_url();?>';
+var myLabel = myLabel || {};
+myLabel.baseUrl = '<?php echo base_url();?>';
 </script>
