38cce6e79baf2fad043c9c396fdcc363bc35797c
soumisleek <soumi@sleekinfosolutions.com>
2020-09-07 19:17:52 +0530

    created country contents page

--- application/modules/app/views/country/index.php
+++ application/modules/app/views/country/index.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-md-5">
                 <div class="city-picture">
-                    <img src="<?php echo base_url('assets/images/paris.jpg');?>" alt="city-image">
+                    <img src="<?php echo base_url('assets/images/paris-new.jpg');?>" alt="city-image">
                 </div>
             </div>
             <div class="col-md-7">
@@ -14,7 +14,120 @@
                         graphic or web designs. The passage is attributed to an unknown typesetter in the 15th
                          century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum
                         for use in a type specimen book.</p>
-                    <p> </p>
+                    <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, 
+                        graphic or web designs. The passage is attributed to an unknown typesetter in the 15th
+                         century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum
+                        for use in a type specimen book </p>
+                        <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, 
+                        graphic or web designs. The passage is attributed to an unknown typesetter in the 15th
+                         century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum
+                        for use in a type specimen book </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section> 
+<!-- 1st part end -->
+<section class="gallery-box">
+    <div class="container">
+        <h3 class="gallery">Gallery</h3>
+        <p class="sub-text">Adventuring with purpose. We believe there's nothing more rewarding than completing a journey together,
+        as you work as a team towards a shared goal - both big challenges and microexpeditions</p>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card blog" >
+                    <img class="img-fluid" src="<?php echo base_url('assets/images/p1.jpg');?>" alt="Card image cap">
+                    <div class="card-body">
+                        <a href="#"><h4>Everest Base Camp Trek</h4></a>
+                        <p>A trip of a lifetime. 16 days of adventure trekking through stunning
+                            landscapes surrounded by the highest mountains in the world.
+                        And you only need to take 9 days off work</p>
+                        <div class="date-price">
+                            <h6>1st - 8th May 2021</h6>
+                            <p>From £240 - 3 spaces remaining</p>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card blog" >
+                    <img class="img-fluid" src="<?php echo base_url('assets/images/p1.jpg');?>" alt="Card image cap">
+                    <div class="card-body">
+                        <a href="#"><h4>Everest Base Camp Trek</h4></a>
+                        <p>A trip of a lifetime. 16 days of adventure trekking through stunning
+                            landscapes surrounded by the highest mountains in the world.
+                        And you only need to take 9 days off work</p>
+                        <div class="date-price">
+                            <h6>1st - 8th May 2021</h6>
+                            <p>From £240 - 3 spaces remaining</p>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card blog" >
+                    <img class="img-fluid" src="<?php echo base_url('assets/images/p1.jpg');?>" alt="Card image cap">
+                    <div class="card-body">
+                        <a href="#"><h4>Everest Base Camp Trek</h4></a>
+                        <p>A trip of a lifetime. 16 days of adventure trekking through stunning
+                            landscapes surrounded by the highest mountains in the world.
+                        And you only need to take 9 days off work</p>
+                        <div class="date-price">
+                            <h6>1st - 8th May 2021</h6>
+                            <p>From £240 - 3 spaces remaining</p>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+             <div class="col-md-4">
+                <div class="card blog" >
+                    <img class="img-fluid" src="<?php echo base_url('assets/images/p1.jpg');?>" alt="Card image cap">
+                    <div class="card-body">
+                        <a href="#"><h4>Everest Base Camp Trek</h4></a>
+                        <p>A trip of a lifetime. 16 days of adventure trekking through stunning
+                            landscapes surrounded by the highest mountains in the world.
+                        And you only need to take 9 days off work</p>
+                        <div class="date-price">
+                            <h6>1st - 8th May 2021</h6>
+                            <p>From £240 - 3 spaces remaining</p>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+             <div class="col-md-4">
+                <div class="card blog" >
+                    <img class="img-fluid" src="<?php echo base_url('assets/images/p1.jpg');?>" alt="Card image cap">
+                    <div class="card-body">
+                        <a href="#"><h4>Everest Base Camp Trek</h4></a>
+                        <p>A trip of a lifetime. 16 days of adventure trekking through stunning
+                            landscapes surrounded by the highest mountains in the world.
+                        And you only need to take 9 days off work</p>
+                        <div class="date-price">
+                            <h6>1st - 8th May 2021</h6>
+                            <p>From £240 - 3 spaces remaining</p>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+             <div class="col-md-4">
+                <div class="card blog" >
+                    <img class="img-fluid" src="<?php echo base_url('assets/images/p1.jpg');?>" alt="Card image cap">
+                    <div class="card-body">
+                        <a href="#"><h4>Everest Base Camp Trek</h4></a>
+                        <p>A trip of a lifetime. 16 days of adventure trekking through stunning
+                            landscapes surrounded by the highest mountains in the world.
+                        And you only need to take 9 days off work</p>
+                        <div class="date-price">
+                            <h6>1st - 8th May 2021</h6>
+                            <p>From £240 - 3 spaces remaining</p>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
         </div>
